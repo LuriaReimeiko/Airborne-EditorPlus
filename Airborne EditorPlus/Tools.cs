726eac3cf75dab34a6130f5915f977f88bb445af
LuriaReimeiko <119699697+LuriaReimeiko@users.noreply.github.com>
2025-08-23 11:20:05 +0200

    Switch to Tabs
    i swear visual has something against me it does not want me to use tabs

--- Airborne EditorPlus/Tools.cs
+++ Airborne EditorPlus/Tools.cs
@@ -4,131 +4,132 @@ using UnityEngine;
 
 namespace Airborne_EditorPlus
 {
-    public class Tools : MelonMod
-    {
-        EditorControls editorControls = EditorPlus.editorControls;
-        public void ChangeActiveState(GameObject obj)
-        {
-            obj.SetActive(!obj.activeSelf);
-        }
+	public class Tools : MelonMod
+	{
+		EditorControls editorControls = EditorPlus.editorControls;
+		public void ChangeActiveState(GameObject obj)
+		{
+			obj.SetActive(!obj.activeSelf);
+		}
 
-        public void SelectObject()
-        {
-            RaycastHit hit;
+		public void SelectObject()
+		{
+			Ray ray = editorControls.mainCam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit2D hit;
+			
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity, editorControls.selectIgnore))
+			{
+				if (Array.IndexOf(editorControls.lastSelectedArray, hit.transform.root.gameObject) != -1) return;
 
-            if (Physics.Raycast(new Vector3(editorControls.mousePos.x, editorControls.mousePos.y, -27), editorControls.transform.forward, out hit, Mathf.Infinity, editorControls.selectIgnore))
-            {
-                if (Array.IndexOf(editorControls.lastSelectedArray, hit.transform.root.gameObject) != -1) return;
+				editorControls.DeselectPortal();
+				editorControls.UIType = EditorControls.uType.none;
+				editorControls.SetUI(true);
 
-                editorControls.DeselectPortal();
-                editorControls.UIType = EditorControls.uType.none;
-                editorControls.SetUI(true);
+				if (Input.GetKey(KeyCode.LeftControl))
+				{
+					editorControls.Select(hit.transform.root.gameObject, true);
+				}
+				else
+				{
+					editorControls.Deselect();
+					editorControls.Select(hit.transform.root.gameObject, false);
+				}
+			}
+		}
 
-                if (Input.GetKey(KeyCode.LeftControl))
-                {
-                    editorControls.Select(hit.transform.root.gameObject, true);
-                }
-                else
-                {
-                    editorControls.Deselect();
-                    editorControls.Select(hit.transform.root.gameObject, false);
-                }
-            }
-        }
+		public void PickObject()
+		{
+			RaycastHit hit;
 
-        public void PickObject()
-        {
-            RaycastHit hit;
+			if (Physics.Raycast(new Vector3(editorControls.mousePos.x, editorControls.mousePos.y, -27), editorControls.transform.forward, out hit, Mathf.Infinity, editorControls.selectIgnore))
+			{
 
-            if (Physics.Raycast(new Vector3(editorControls.mousePos.x, editorControls.mousePos.y, -27), editorControls.transform.forward, out hit, Mathf.Infinity, editorControls.selectIgnore))
-            {
+				if (Array.IndexOf(editorControls.lastSelectedArray, hit.transform.root.gameObject) != -1) return;
 
-                if (Array.IndexOf(editorControls.lastSelectedArray, hit.transform.root.gameObject) != -1) return;
+				editorControls.DeselectPortal();
+				editorControls.Deselect();
+				editorControls.selectedObject = false;
 
-                editorControls.DeselectPortal();
-                editorControls.Deselect();
-                editorControls.selectedObject = false;
+				// There seems to be no better way as this is how the game is made
+				string name = hit.transform.root.gameObject.name;
+				if (name.Contains("Platform"))
+					editorControls.ToggleButton(1);
+				else if (name.Contains("Spike"))
+					editorControls.ToggleButton(2);
+				else if (name.Contains("button"))
+					editorControls.ToggleButton(3);
+				else if (name.Contains("Turret"))
+					editorControls.ToggleButton(4);
+				else if (name.Contains("Laser"))
+					editorControls.ToggleButton(6); // why is only laser
+				else if (name.Contains("Launch Pad"))
+					editorControls.ToggleButton(5); // and launch pad inverted
+				else if (name.Contains("text"))
+					editorControls.ToggleButton(7);
+				else if (name.Contains("Teleporter"))
+					editorControls.ToggleButton(8);
+				else if (name.Contains("Checkpoint"))
+					editorControls.ToggleButton(9);
+			}
+		}
 
-                // There seems to be no better way as this is how the game is made
-                string name = hit.transform.root.gameObject.name;
-                if (name.Contains("Platform"))
-                    editorControls.ToggleButton(1);
-                else if (name.Contains("Spike"))
-                    editorControls.ToggleButton(2);
-                else if (name.Contains("button"))
-                    editorControls.ToggleButton(3);
-                else if (name.Contains("Turret"))
-                    editorControls.ToggleButton(4);
-                else if (name.Contains("Laser"))
-                    editorControls.ToggleButton(6); // why is only laser
-                else if (name.Contains("Launch Pad"))
-                    editorControls.ToggleButton(5); // and launch pad inverted
-                else if (name.Contains("text"))
-                    editorControls.ToggleButton(7);
-                else if (name.Contains("Teleporter"))
-                    editorControls.ToggleButton(8);
-                else if (name.Contains("Checkpoint"))
-                    editorControls.ToggleButton(9);
-            }
-        }
+		public void DeselectObject()
+		{
+			editorControls.Deselect();
+			editorControls.DeselectPortal();
+			editorControls.SetUI(false);
+		}
 
-        public void DeselectObject()
-        {
-            editorControls.Deselect();
-            editorControls.DeselectPortal();
-            editorControls.SetUI(false);
-        }
+		public void SetObjectColor(GameObject obj, Color color)
+		{
+			obj.GetComponent<MeshRenderer>().material.color = color;
+			//Material coloredMat = new Material(new Shader());
+			//coloredMat.color = color;
+			//obj.GetComponent<MeshRenderer>().material = coloredMat;
+		}
 
-        public void SetObjectColor(GameObject obj, Color color)
-        {
-            obj.GetComponent<MeshRenderer>().material.color = color;
-            //Material coloredMat = new Material(new Shader());
-            //coloredMat.color = color;
-            //obj.GetComponent<MeshRenderer>().material = coloredMat;
-        }
+		public void Pan()
+		{
+			EditorControls.uType lastuType = EditorControls.uType.none;
+			bool lastSelected = false;
 
-        public void Pan()
-        {
-            EditorControls.uType lastuType = EditorControls.uType.none;
-            bool lastSelected = false;
+			if (Input.GetMouseButtonDown(0))
+			{
+				lastuType = editorControls.UIType;
+				editorControls.UIType = EditorControls.uType.none;
+				editorControls.SetUI(true);
 
-            if (Input.GetMouseButtonDown(0))
-            {
-                lastuType = editorControls.UIType;
-                editorControls.UIType = EditorControls.uType.none;
-                editorControls.SetUI(true);
+				lastSelected = editorControls.selectedObject;
+			}
+			if (Input.GetMouseButton(0))
+			{
+				Vector3 pos;
+				pos.x = Mathf.Clamp(editorControls.mainCam.transform.position.x - (Input.GetAxisRaw("Mouse X") / (1.5f * (editorControls.mainCam.fieldOfView / 36))), -86.1f, 86.1f);
+				pos.y = Mathf.Clamp(editorControls.mainCam.transform.position.y - (Input.GetAxisRaw("Mouse Y") / (1.5f * (editorControls.mainCam.fieldOfView / 36))), -78, 78);
+				pos.z = -32;
+				editorControls.mainCam.transform.position = pos;
+			}
+			if (Input.GetMouseButtonUp(0))
+			{
+				editorControls.UIType = lastuType;
+				editorControls.SetUI(true);
 
-                lastSelected = editorControls.selectedObject;
-            }
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 pos;
-                pos.x = Mathf.Clamp(editorControls.mainCam.transform.position.x - (Input.GetAxisRaw("Mouse X") / (1.5f * (editorControls.mainCam.fieldOfView / 36))), -86.1f, 86.1f);
-                pos.y = Mathf.Clamp(editorControls.mainCam.transform.position.y - (Input.GetAxisRaw("Mouse Y") / (1.5f * (editorControls.mainCam.fieldOfView / 36))), -78, 78);
-                pos.z = -32;
-                editorControls.mainCam.transform.position = pos;
-            }
-            if (Input.GetMouseButtonUp(0))
-            {
-                editorControls.UIType = lastuType;
-                editorControls.SetUI(true);
+				editorControls.selectedObject = lastSelected;
+			}
+		}
 
-                editorControls.selectedObject = lastSelected;
-            }
-        }
+		public void HandleZoom()
+		{
+			if (!editorControls.cantScroll && !Input.GetKey(KeyCode.LeftControl))
+			{
+				Vector3 pos;
+				pos.x = Mathf.Clamp(editorControls.mainCam.transform.position.x - (Input.GetAxisRaw("Mouse X") * Input.GetAxisRaw("Mouse ScrollWheel")), -86.1f, 86.1f);
+				pos.y = Mathf.Clamp(editorControls.mainCam.transform.position.y - (Input.GetAxisRaw("Mouse Y") * Input.GetAxisRaw("Mouse ScrollWheel")), -78, 78);
+				pos.z = -32;
 
-        public void HandleZoom()
-        {
-            if (!editorControls.cantScroll && !Input.GetKey(KeyCode.LeftControl))
-            {
-                Vector3 pos;
-                pos.x = Mathf.Clamp(editorControls.mainCam.transform.position.x - (Input.GetAxisRaw("Mouse X") * Input.GetAxisRaw("Mouse ScrollWheel")), -86.1f, 86.1f);
-                pos.y = Mathf.Clamp(editorControls.mainCam.transform.position.y - (Input.GetAxisRaw("Mouse Y") * Input.GetAxisRaw("Mouse ScrollWheel")), -78, 78);
-                pos.z = -32;
+				editorControls.mainCam.transform.position = pos;
 
-                editorControls.mainCam.transform.position = pos;
-
-            }
-        }
-    }
+			}
+		}
+	}
 }
